49842b29364f8c1ae365267218cd226a132bf092
Sahil <sahil.dixit15.sd@gmail.com>
2022-12-25 16:24:54 +0530

    first commit

---
+++ public/assets/index.js
@@ -0,0 +1,44 @@
+const btn1 = document.getElementById("btn");
+btn1.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp1.html`;
+});
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp2.html`;
+});
+const btn3 = document.getElementById("btn3");
+btn3.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp3.html`;
+});
+const btn4 = document.getElementById("btn4");
+btn4.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp4.html`;
+});
+const btn5 = document.getElementById("btn5");
+btn5.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp5.html`;
+});
+const btn6 = document.getElementById("btn6");
+btn6.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp6.html`;
+});
+const btn7 = document.getElementById("btn7");
+btn7.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp7.html`;
+});
+const btn8 = document.getElementById("btn8");
+btn8.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp8.html`;
+});
+const btn9 = document.getElementById("btn9");
+btn9.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp9.html`;
+});
+const btn10 = document.getElementById("btn10");
+btn10.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp10.html`;
+});
+const btn11 = document.getElementById("btn11");
+btn11.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp11.html`;
+});
