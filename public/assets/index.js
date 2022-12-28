bbab523da7e66efa703f1de52f462cc43e453d13
Sahil <sahil.dixit15.sd@gmail.com>
2022-12-28 22:32:56 +0530

    reverted back to wt

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,146 +1,146 @@
-// const btn1 = document.getElementById("btn");
-// btn1.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp1.html`;
-// });
-// const btn2 = document.getElementById("btn2");
-// btn2.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp2.html`;
-// });
-// const btn3 = document.getElementById("btn3");
-// btn3.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp3.html`;
-// });
-// const btn4 = document.getElementById("btn4");
-// btn4.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp4.html`;
-// });
-// const btn5 = document.getElementById("btn5");
-// btn5.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp5.html`;
-// });
-// const btn6 = document.getElementById("btn6");
-// btn6.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp6.html`;
-// });
-// const btn7 = document.getElementById("btn7");
-// btn7.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp7.html`;
-// });
-// const btn8 = document.getElementById("btn8");
-// btn8.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp8.html`;
-// });
-// const btn9 = document.getElementById("btn9");
-// btn9.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp9.html`;
-// });
-// const btn10 = document.getElementById("btn10");
-// btn10.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp10.html`;
-// });
-// const btn11 = document.getElementById("btn11");
-// btn11.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp11.html`;
-// });
-// const btn14 = document.getElementById("btn14");
-// btn14.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp14.html`;
-// });
-// const btn15 = document.getElementById("btn15");
-// btn15.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp15.html`;
-// });
-// const btn16 = document.getElementById("btn16");
-// btn16.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp16.html`;
-// });
-// const btn17 = document.getElementById("btn17");
-// btn17.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp17.html`;
-// });
-// const btn18 = document.getElementById("btn18");
-// btn18.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp18.html`;
-// });
-// const btn19 = document.getElementById("btn19");
-// btn19.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp19.html`;
-// });
-// const btn20 = document.getElementById("btn20");
-// btn20.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp20.html`;
-// });
-// const btn21 = document.getElementById("btn21");
-// btn21.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp21.html`;
-// });
-// const btn22 = document.getElementById("btn22");
-// btn22.addEventListener("click", function () {
-//     window.location.href = `./allCodes/exp22.html`;
-// });
-
-
-const btn1 = document.getElementById("btn1");
+const btn1 = document.getElementById("btn");
 btn1.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp1.html`;
+    window.location.href = `./allCodes/exp1.html`;
 });
 const btn2 = document.getElementById("btn2");
 btn2.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp2.html`;
+    window.location.href = `./allCodes/exp2.html`;
 });
 const btn3 = document.getElementById("btn3");
 btn3.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp3.html`;
+    window.location.href = `./allCodes/exp3.html`;
 });
 const btn4 = document.getElementById("btn4");
 btn4.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp4.html`;
+    window.location.href = `./allCodes/exp4.html`;
 });
 const btn5 = document.getElementById("btn5");
 btn5.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp5.html`;
+    window.location.href = `./allCodes/exp5.html`;
 });
 const btn6 = document.getElementById("btn6");
 btn6.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp6.html`;
+    window.location.href = `./allCodes/exp6.html`;
 });
 const btn7 = document.getElementById("btn7");
 btn7.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp7.html`;
+    window.location.href = `./allCodes/exp7.html`;
 });
 const btn8 = document.getElementById("btn8");
 btn8.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp8.html`;
+    window.location.href = `./allCodes/exp8.html`;
 });
 const btn9 = document.getElementById("btn9");
 btn9.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp9.html`;
+    window.location.href = `./allCodes/exp9.html`;
 });
 const btn10 = document.getElementById("btn10");
 btn10.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp10.html`;
+    window.location.href = `./allCodes/exp10.html`;
 });
 const btn11 = document.getElementById("btn11");
 btn11.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp11.html`;
-});
-const btn12 = document.getElementById("btn12");
-btn12.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp12.html`;
-});
-const btn13 = document.getElementById("btn13");
-btn13.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp13.html`;
+    window.location.href = `./allCodes/exp11.html`;
 });
 const btn14 = document.getElementById("btn14");
 btn14.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp14.html`;
-})
+    window.location.href = `./allCodes/exp14.html`;
+});
 const btn15 = document.getElementById("btn15");
 btn15.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp15.html`;
+    window.location.href = `./allCodes/exp15.html`;
 });
 const btn16 = document.getElementById("btn16");
 btn16.addEventListener("click", function () {
-    window.location.href = `./allCodes2/exp16.html`;
+    window.location.href = `./allCodes/exp16.html`;
 });
+const btn17 = document.getElementById("btn17");
+btn17.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp17.html`;
+});
+const btn18 = document.getElementById("btn18");
+btn18.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp18.html`;
+});
+const btn19 = document.getElementById("btn19");
+btn19.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp19.html`;
+});
+const btn20 = document.getElementById("btn20");
+btn20.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp20.html`;
+});
+const btn21 = document.getElementById("btn21");
+btn21.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp21.html`;
+});
+const btn22 = document.getElementById("btn22");
+btn22.addEventListener("click", function () {
+    window.location.href = `./allCodes/exp22.html`;
+});
+
+
+// const btn1 = document.getElementById("btn1");
+// btn1.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp1.html`;
+// });
+// const btn2 = document.getElementById("btn2");
+// btn2.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp2.html`;
+// });
+// const btn3 = document.getElementById("btn3");
+// btn3.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp3.html`;
+// });
+// const btn4 = document.getElementById("btn4");
+// btn4.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp4.html`;
+// });
+// const btn5 = document.getElementById("btn5");
+// btn5.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp5.html`;
+// });
+// const btn6 = document.getElementById("btn6");
+// btn6.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp6.html`;
+// });
+// const btn7 = document.getElementById("btn7");
+// btn7.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp7.html`;
+// });
+// const btn8 = document.getElementById("btn8");
+// btn8.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp8.html`;
+// });
+// const btn9 = document.getElementById("btn9");
+// btn9.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp9.html`;
+// });
+// const btn10 = document.getElementById("btn10");
+// btn10.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp10.html`;
+// });
+// const btn11 = document.getElementById("btn11");
+// btn11.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp11.html`;
+// });
+// const btn12 = document.getElementById("btn12");
+// btn12.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp12.html`;
+// });
+// const btn13 = document.getElementById("btn13");
+// btn13.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp13.html`;
+// });
+// const btn14 = document.getElementById("btn14");
+// btn14.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp14.html`;
+// })
+// const btn15 = document.getElementById("btn15");
+// btn15.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp15.html`;
+// });
+// const btn16 = document.getElementById("btn16");
+// btn16.addEventListener("click", function () {
+//     window.location.href = `./allCodes2/exp16.html`;
+// });
